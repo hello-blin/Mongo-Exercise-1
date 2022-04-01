e647f6608e4033e3925a9df9fc7844b43839d8c6
Blin Kukaj <kblin164@gmail.com>
2022-04-01 04:19:52 +0200

    Got some data for my local db

--- app.js
+++ app.js
@@ -2,27 +2,33 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
+mongoose
+  .connect("mongodb://localhost:27017/exercise-1")
+  .then(() => {
+    console.log("Connected to Mongo");
+  })
+  .catch((err) => console.log(err));
+
 const courseSchema = new mongoose.Schema({
   name: String,
   author: String,
-  price: Number,
   tags: [String],
+  date: Date,
   isPublished: Boolean,
+  price: Number,
 });
 
-const Course = new mongoose.model("Course", courseSchema);
+const Course = mongoose.model("Course", courseSchema);
 
-
-
-//EXERCISE 1 
+//EXERCISE 1
 
 // Get all the published Backend Courses,
 // Sort them by their name,
 // Pick only their name and author,
 // Display them on console
 
-async function getCourse(){
-  const course = await Course.find()
+async function getCourse() {
+  const course = await Course.find();
 }
 
 app.listen(3000, () => {
